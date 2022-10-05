9569b4bf40209f9e4f4b85639d5f0a385f232788
Vladimir Bezgodov <vb@invoice.su>
2022-10-05 23:06:57 +0700

    Add utf-8

--- bitrix/modules/invoice.payment.utf8/invoice/.description.php
+++ bitrix/modules/invoice.payment.utf8/invoice/.description.php
@@ -2,8 +2,8 @@
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 include(GetLangFileName(dirname(__FILE__)."/", "/payment.php"));
 
-$psTitle = "Invoice (UTF-8)";
-$psDescription = "Платежная система Invoice (UTF-8)";
+$psTitle = "Invoice";
+$psDescription = "Платежная система Invoice";
 $arPSCorrespondence = array(
     "ORDER_ID" => array(
         "NAME" => GetMessage("ORDER_ID"),
